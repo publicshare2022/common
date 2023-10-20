4d54e54a55c4a2afb4f419e4a33489601cf66788
skiplee <skiplee85@gmail.com>
2023-10-20 20:03:13 +0800

    fix: 添加grpc错误处理

--- xerr/resp.go
+++ xerr/resp.go
@@ -5,15 +5,18 @@ import (
 	"net/http"
 
 	"github.com/zeromicro/go-zero/rest/httpx"
+	"google.golang.org/grpc/status"
 )
 
 func Response(ctx context.Context, w http.ResponseWriter, resp interface{}, err error) {
 	body := NewErrorCode(OK)
 	if err != nil {
-		if x, ok := err.(*Error); !ok {
-			body = NewError(UNKNOW_ERROR, err.Error())
-		} else {
+		if x, ok := err.(*Error); ok {
 			body = x
+		} else if x, ok := status.FromError(err); ok {
+			body = NewError(uint32(x.Code()), x.Message())
+		} else {
+			body = NewError(UNKNOW_ERROR, err.Error())
 		}
 	} else {
 		body.Data = resp
