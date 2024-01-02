b90a5fff062fcf2f550b2290e68494a8a9efbc19
skiplee <skiplee85@gmail.com>
2024-01-02 12:43:43 +0800

    feat: 自定义结构解析

--- xerr/code.go
+++ xerr/code.go
@@ -1,12 +1,10 @@
 package xerr
 
-//成功返回
+// 成功返回
 const OK uint32 = 0
 
-//全局错误码
+// 全局错误码
 const UNKNOW_ERROR uint32 = 99
 const SERVER_COMMON_ERROR uint32 = 500
 const REUQEST_PARAM_ERROR uint32 = 400
 const TOKEN_EXPIRE_ERROR uint32 = 401
-
-/**(前3位代表业务,后三位代表具体功能)**/
---
+++ xerr/req.go
@@ -0,0 +1,36 @@
+package xerr
+
+import (
+	"mime"
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func Parse(r *http.Request, v any) error {
+
+	if err := httpx.ParsePath(r, v); err != nil {
+		return err
+	}
+
+	if err := httpx.ParseHeaders(r, v); err != nil {
+		return err
+	}
+
+	ct := r.Header.Get("Content-Type")
+	d, _, err := mime.ParseMediaType(ct)
+	if err != nil {
+		return err
+	}
+	if d == "application/json" {
+		if err := httpx.ParseJsonBody(r, v); err != nil {
+			return err
+		}
+	} else {
+		if err := httpx.ParseForm(r, v); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
